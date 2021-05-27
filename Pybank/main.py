302283ba32ece08fbbfb68d8e9fbf2e1160b21cc
Dinh Chau <dinhchau001@gmail.com>
2021-05-26 21:52:23 -0700

    Continued with the opening of the CSV file

--- Pybank/main.py
+++ Pybank/main.py
@@ -9,3 +9,17 @@ change = 0
 dates = []
 profits = []
 
+
+with open("budget_data.csv", "r") as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter =",")
+    csv_header = next(csv_reader)
+    file_to_output = "budget_analysis.txt"
+
+
+       for row in csv_reader:
+        
+        total_months = total_months + 1  #
+        
+        total_revenue = total_revenue + int(row[1]) 
+        revenue.append(row[1]) 
+        date.append(row[0])  
