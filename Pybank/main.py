62d8e815fe91e9b3e241bfe3d12c2494316136ff
Dinh Chau <dinhchau001@gmail.com>
2021-05-27 16:55:44 -0700

    Reading row and removal of revenue variable

--- Pybank/main.py
+++ Pybank/main.py
@@ -3,23 +3,39 @@ import csv
 filepath = os.path.join("..", "Pybank","budget_data.csv")
 
 total_months = 0
-total_pl = 0
+total_profit_loss = 0 #profit/loss
 value = 0
 change = 0
 dates = []
 profits = []
 
 
-with open("budget_data.csv", "r") as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter =",")
+with open("budget_data.csv", "r") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter =",")
     csv_header = next(csv_reader)
     file_to_output = "budget_analysis.txt"
 
 
+    total_months = total_months + 1
+   
+    total_profit_loss = total_profit_loss + int(first_row[1])
+  
+    value = int(first_row[1])
+      
+      
        for row in csv_reader:
         
-        total_months = total_months + 1  #
-        
-        total_revenue = total_revenue + int(row[1]) 
-        revenue.append(row[1]) 
-        date.append(row[0])  
+        total_months = total_months + 1 
+        change = int(row[1])-value
+        total_profit_loss = total_profit_loss + int(row[1]) 
+        profits.append(row[1]) 
+        value = int(row[1])
+        dates.append(row[0])  
+
+        Greatest_Decrease = min[profits]
+
+        Great_Increase = max[profits]
+
+    
+
+
