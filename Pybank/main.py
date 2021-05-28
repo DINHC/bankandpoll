6aff000d77d7527e5fdd850d9f3aba67f9fe0374
Dinh Chau <dinhchau001@gmail.com>
2021-05-28 15:49:08 -0700

    Added average, increase/decrease, and print but the average function continues having error

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,6 +1,6 @@
 import os
 import csv
-filepath = os.path.join("..", "Pybank","budget_data.csv") 
+file_path = os.path.join("Resources","budget_data.csv") 
 
 total_months = 0
 total_profit_loss = 0 #profit/loss (change name maybe?)
@@ -10,11 +10,11 @@ dates = []
 profits = []
 
 
-with open("budget_data.csv", "r") as csv_file:
+with open(file_path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter =",")
     csv_header = next(csv_reader)
     file_to_output = "budget_analysis.txt"
-
+    first_row = next(csv_reader)
 
     total_months = total_months + 1
    
@@ -23,29 +23,38 @@ with open("budget_data.csv", "r") as csv_file:
     value = int(first_row[1])
       
       
-       for row in csv_reader:
+    for row in csv_reader:
         
         total_months = total_months + 1 
         change = int(row[1])-value
         total_profit_loss = total_profit_loss + int(row[1]) 
-        profits.append(row[1]) 
+        profits.append((row[1])) 
         value = int(row[1])
         dates.append(row[0])  
 
-        average = sum(profits)/len(profits)
+        
+        average = sum(profits)/len(profits) # error here regarding operands check in eralier line if there is misspelling
 
         Greatest_Decrease = min(profits)
         Lowest_Index = profits.index(Greatest_Decrease)
-        Lowest_Date = dates(Lowest_Index)
+        Lowest_Date = dates[Lowest_Index]
 
         Greatest_Increase = max(profits)
         Greatest_Index = profits.index(Greatest_Increase)
-        Greatest_Date = dates(Greatest_Index)
+        Greatest_Date = dates[Greatest_Index]
 
 
 
+print("Financial Analysis")
+print("------------------")
+print(f"Total Months: {str(total_months)}")
+print(f"Total: ${str(total_profit_loss)}")
+print(f"Average Change: ${str(round(average,2))}")
+print(f"Greatest Increase in Profits: {Greatest_Date} (${str(Greatest_Increase)})")
+print(f"Greatest Decrease in Profits: {Lowest_Date} (${str(Greatest_Decrease)})")
 
         
+os.getcwd
 
 
     
