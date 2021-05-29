bd0a318cff0291e693d95d7fc349a88c707fc418
Dinh Chau <dinhchau001@gmail.com>
2021-05-29 13:36:10 -0700

    Check out line 31, changed value variable, neeed to still work on txt file export

--- Pybank/main.py
+++ Pybank/main.py
@@ -4,7 +4,7 @@ file_path = os.path.join("Resources","budget_data.csv")
 
 total_months = 0
 total_profit_loss = 0 #profit/loss (change name maybe?)
-value = 0
+month_total = 0
 change = 0
 dates = []
 profits = []
@@ -16,22 +16,28 @@ with open(file_path) as csv_file:
     file_to_output = "budget_analysis.txt"
     first_row = next(csv_reader)
 
-    total_months = total_months + 1
+    total_months += 1
+    #total_months = total_months + 1
    
-    total_profit_loss = total_profit_loss + int(first_row[1])
+    total_profit_loss += int(first_row[1])
+    #total_profit_loss = total_profit_loss + int(first_row[1])
   
-    value = int(first_row[1])
+    month_total = int(first_row[1])
       
       
     for row in csv_reader:
         
         total_months = total_months + 1 
-        change = int(row[1])-value
+        change = int(row[1])-month_total #
         total_profit_loss = total_profit_loss + int(row[1]) 
         profits.append(int(row[1])) 
         value = int(row[1])
         dates.append(row[0])  
 
+        # net change = current month amount - past month amount
+        # past month amount = int(row[1])
+        # change list += net change
+        # monthly average = sum(change list)/len(change list)
         
         average = sum(profits)/len(profits) # error here regarding operands check in eralier line if there is misspelling
 
@@ -65,7 +71,6 @@ text_export.write(f"Total: ${str(total_profit_loss)}")
 text_export.write(f"Average Change: ${str(round(average,2))}")
 text_export.write(f"Greatest Increase in Profits: {Greatest_Date} (${str(Greatest_Increase)})")
 text_export.write(f"Greatest Decrease in Profits: {Lowest_Date} (${str(Greatest_Decrease)})")
-text_export.write()
 text_export.close()
 
 
