e390da930aba18840b190e4a3b879f9dfd0b9f4e
Dinh Chau <dinhchau001@gmail.com>
2021-05-31 16:03:26 -0700

    Added csv reader

--- Pypoll/main(Pypoll).py
+++ Pypoll/main(Pypoll).py
@@ -6,3 +6,12 @@ election = os.path.join("Resources", "election_data.csv")
 candidates = []
 vote_count = []
 vote_percentage = []
+vote_amount = 0 
+
+    with open(election) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter =",")
+        csv_header = next(csv_reader)
+        file_to_output = "election_data.txt"
+   
+        for row in csv_reader: 
+            
