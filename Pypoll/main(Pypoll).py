8f9b59d2393ea88fa4cf4e9171ae24495371c52e
Dinh Chau <dinhchau001@gmail.com>
2021-06-01 14:12:34 -0700

    Fixed issue with wrong variable placement, print works, and finally got the print to export to Analysis folder

--- Pypoll/main(Pypoll).py
+++ Pypoll/main(Pypoll).py
@@ -9,28 +9,25 @@ vote_percentage = []
 vote_amount = 0 
 
 with open(election) as csvfile:
-        csv_reader = csv.reader(csvfile, delimiter =",")
+        csv_reader = csv.reader(csvfile, delimiter = ",")
         csv_header = next(csv_reader)
         file_to_output = "election_data.txt"
    
         for row in csv_reader:  
             vote_amount += 1 
         
-        if row[2] not in candidates:
-            candidates.append(row[2])
-            index = candidates.index(row[2])
-            vote_count.append(1)
-        else:
-            index = candidates.index(row[2])
-            vote_count[index] += 1
+            if row[2] not in candidates:
+                candidates.append(row[2])
+                index = candidates.index(row[2])
+                vote_count.append(1)
+            else:
+                index = candidates.index(row[2])
+                vote_count[index] += 1
         
-
-
-
 for votes in vote_count: 
     percent = (votes/vote_amount) * 100 
     percent = round(percent)
-    percent = "{:.2%}".format(percent)
+    percent = "%.3f%%" % percent
     vote_percentage.append(percent)
 
 winner = max(vote_count)
@@ -46,3 +43,17 @@ for i in range(len(candidates)):
 print("--------------------------")
 print(f"Winner: {win_candidate}")
 print("--------------------------")
+
+
+text_export = open(os.path.join("..","Pypoll","Analysis", "Pypoll.txt"), "w")
+
+text_export.write("Election Results\n")
+text_export.write("------------------\n")
+text_export.write(f"Total Votes: {str(vote_amount)}\n")
+text_export.write("------------------\n")
+for i in range(len(candidates)):
+    text_export.write(f"{candidates[i]}: {str(vote_percentage[i])} ({str(vote_count[i])})\n")
+text_export.write("--------------------\n")
+text_export.write(f"Winner: {win_candidate}\n")
+text_export.write("--------------------\n")
+text_export.close()
